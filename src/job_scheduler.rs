385ba0ddd3fe52ac9b932de32d84c76b94d57b3b
Junichi Kato <j5ik2o@gmail.com>
2023-11-30 19:35:00 +0900

    reformat

--- src/job_scheduler.rs
+++ src/job_scheduler.rs
@@ -1,83 +1,82 @@
-use crate::job::{Job, JobContext};
 use ulid_generator_rs::{ULIDGenerator, ULID};
+
+use crate::job::{Job, JobContext};
 
 pub struct JobScheduler<F, T>
 where
-    F: FnMut(JobContext<T>),
-{
-    id: ULID,
-    jobs: Vec<Job<F, T>>,
+  F: FnMut(JobContext<T>), {
+  id: ULID,
+  jobs: Vec<Job<F, T>>,
 }
 
 impl<F, T> JobScheduler<F, T>
 where
-    F: FnMut(JobContext<T>),
+  F: FnMut(JobContext<T>),
 {
-    pub fn new() -> Self {
-        let mut generator = ULIDGenerator::new();
-        let id = generator.generate().unwrap();
-        JobScheduler {
-            id,
-            jobs: Vec::new(),
-        }
-    }
+  pub fn new() -> Self {
+    let mut generator = ULIDGenerator::new();
+    let id = generator.generate().unwrap();
+    JobScheduler { id, jobs: Vec::new() }
+  }
 
-    pub fn add_job(&mut self, job: Job<F, T>) {
-        self.jobs.push(job);
-    }
+  pub fn add_job(&mut self, job: Job<F, T>) {
+    self.jobs.push(job);
+  }
 
-    pub fn tick(&mut self) {
-        for job in self.jobs.iter_mut() {
-            job.tick();
-        }
+  pub fn tick(&mut self) {
+    for job in self.jobs.iter_mut() {
+      job.tick();
     }
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use chrono::{Duration, Utc};
-    use std::env;
-    use std::thread::sleep;
+  use std::env;
+  use std::thread::sleep;
 
-    #[test]
-    fn test_tick() {
-        env::set_var("RUST_LOG", "debug");
-        let _ = env_logger::builder().is_test(true).try_init();
+  use chrono::{Duration, Utc};
 
-        let mut job_scheduler = JobScheduler::new();
+  use super::*;
 
-        let tick_interval = Duration::seconds(1);
-        let mut counter = 0;
+  #[test]
+  fn test_tick() {
+    env::set_var("RUST_LOG", "debug");
+    let _ = env_logger::builder().is_test(true).try_init();
 
-        let job = Job::new(
-            "*/1 * * * *".to_string(),
-            |job_context| {
-                let str = job_context.data().borrow().unwrap();
-                log::debug!(
-                    "schedule_datetime = {}, now = {}: {}) {}",
-                    job_context.trigger(),
-                    job_context.now(),
-                    counter,
-                    str
-                );
-                counter += 1;
-            },
-            Some("Hello, world!"),
-        )
-        .with_tick_interval(tick_interval);
-        job_scheduler.add_job(job);
+    let mut job_scheduler = JobScheduler::new();
 
-        let now = Utc::now();
-        let end_time = now + Duration::seconds(2);
+    let tick_interval = Duration::seconds(1);
+    let mut counter = 0;
 
-        while Utc::now() < end_time {
-            log::debug!("tick: now = {}", Utc::now());
-            job_scheduler.tick();
-            log::debug!("waiting for {} seconds...", tick_interval.num_seconds());
-            sleep(tick_interval.to_std().unwrap());
-        }
+    let job = Job::new(
+      "*/1 * * * *".to_string(),
+      |job_context| {
+        let str = job_context.data().borrow().unwrap();
+        log::debug!(
+          "schedule_datetime = {}, now = {}: {}) {}",
+          job_context.trigger(),
+          job_context.now(),
+          counter,
+          str
+        );
+        counter += 1;
+      },
+      Some("Hello, world!"),
+    )
+    .with_tick_interval(tick_interval);
+    job_scheduler.add_job(job);
 
-        assert_eq!(counter, 1);
+    let now = Utc::now();
+    let end_time = now + Duration::seconds(2);
+
+    while Utc::now() < end_time {
+      log::debug!("tick: now = {}", Utc::now());
+      job_scheduler.tick();
+      log::debug!("waiting for {} seconds...", tick_interval.num_seconds());
+      sleep(tick_interval.to_std().unwrap());
     }
+
+    assert_eq!(counter, 1);
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@ pub mod job;
 pub mod job_scheduler;
 
 pub fn add(left: usize, right: usize) -> usize {
-    left + right
+  left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+  }
 }
